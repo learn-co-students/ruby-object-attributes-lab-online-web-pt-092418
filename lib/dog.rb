d8a6c979f6505c8464e856c95a184a80c8429bee
Patrick Shim <patrickshim@Patricks-MBP.fios-router.home>
2018-10-18 13:16:45 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-  def name= (name)
-    @name= name
-  end
-  
   def name
     @name
   end
-  
-  def breed= (breed)
-    @breed= breed
+
+  def name=(new_name)
+    @name = new_name
   end
-  
+
   def breed
     @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
   end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,17 @@
 class Person
-  def name= (name)
-    @name= name
-  end
-  
   def name
     @name
   end
-  
-  def job= (job)
-    @job= job
+
+  def name=(new_name)
+    @name = new_name
   end
-    
+
   def job
     @job
   end
-end
 
-  
-  
+  def job= (new_job)
+    @job = new_job
+  end
+end
